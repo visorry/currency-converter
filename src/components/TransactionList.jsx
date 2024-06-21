8d9193115f7c60047b75ba62dfa7d77b5c7c9a82
Vishnu R <vishnuvisa00@gmail.com>
2024-06-21 14:01:19 +0530

    Fix encoding issue in CurrencySelector component

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import './TransactionList.css'; 
 
 const TransactionList = ({ transactions }) => (
   <div>
-    <h3>Last 5 Transactions</h3>
-    <ul>
-      {transactions.map((trans, index) => (
-        <li key={index}>{trans}</li>
-      ))}
+    <h3>Recent 5 Transactions</h3>
+    <ul className="transaction-list">
+      {transactions.map((trans, index) => {
+        const [transactionDetails, date] = trans.split('|'); // Splitting transaction and date
+        return (
+          <li key={index}>
+            <span className="transaction-details">{transactionDetails}</span>
+            <span className="transaction-date">{date}</span>
+          </li>
+        );
+      })}
     </ul>
   </div>
 );
